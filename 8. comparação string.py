14330393b8d56a573f55503b9efb8bdd9a3b7a8b
gabrielhuervo <gabriel.tavares112@outlook.com>
2023-03-08 17:46:24 -0300

    3.1.1.10 LAB: Comparison operators and conditional execution

---
+++ 8. comparação string.py
@@ -0,0 +1,8 @@
+nome = input("Digite o nome da flor: ")
+
+if nome == "Spathiphyllum":
+    print("Sim - Spathiphyllum é a melhor planta de todas!")
+elif nome == "spathiphyllum":
+    print("Não, eu quero uma grande Spathiphyllum!")
+else:
+    print("Spathiphyllum! Não", nome + "!")
