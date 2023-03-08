f5e9ab05df9f58b6a41e80852c083240e06ef479
gabrielhuervo <gabriel.tavares112@outlook.com>
2023-03-08 17:50:37 -0300

    3.1.1.12 LAB: Essentials of the if-elif-else statement

--- 9. imposto.py
+++ 9. imposto.py
@@ -1,12 +1,12 @@
-income = float(input("Digite o rendimento anual: "))
+renda = float(input("Digite o rendimento anual: "))
 
-if income < 85528:
-    tax = income * 0.18 - 556.02
+if renda < 85528:
+    imposto = renda * 0.18 - 556.02
 else:
-    tax = (income - 85528) * 0.32 + 14839.02
+    imposto = (renda - 85528) * 0.32 + 14839.02
 
-if tax < 0.0:
-    tax = 0.0
+if imposto < 0.0:
+    imposto = 0.0
 
-tax = round(tax, 0)
-print("O imposto é:", tax, "thalers")
+imposto = round(imposto, 0)
+print("O imposto é:", imposto, "thalers")
