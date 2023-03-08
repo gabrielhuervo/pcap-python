25de7cf416f0572e721f9592a59f35190255be61
gabrielhuervo <gabriel.tavares112@outlook.com>
2023-03-08 18:08:02 -0300

    3.4.1.6 LAB: The basics of lists

---
+++ 17. lista.py
@@ -0,0 +1,11 @@
+# Criando a lista de chapéus
+hat_list = [1, 2, 3, 4, 5]
+
+# Passo 1
+hat_list[2] = int(input("Digite um número inteiro: "))
+
+# Passo 2
+del hat_list[-1]
+
+# Passo 3
+print(len(hat_list))
