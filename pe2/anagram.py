548dd925ce09ba87df9a91dade39e143b5a16938
gabrielhuervo <gabriel.tavares112@outlook.com>
2023-03-10 11:02:45 -0300

    2.5.1.8 LAB: Anagrams

---
+++ pe2/anagram.py
@@ -0,0 +1,9 @@
+str_1 = input("First string: ")
+str_2 = input("Second string: ")
+
+strx_1 = ''.join(sorted(list(str_1.upper().replace(' ',''))))
+strx_2 = ''.join(sorted(list(str_2.upper().replace(' ',''))))
+if len(strx_1) > 0 and strx_1 == strx_2:
+	print("Anagram")
+else:
+	print("Not anagram")
